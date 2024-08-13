46dbc8c24670addf649f11d8992612e77669eefc
frustrated018 <niloyd018@gmail.com>
2024-08-13 21:29:42 +0600

    Updated colors and font
    updated the tailwind theme to green
    Added custom colors that match the theme in tailwind with the `si` prefix
    Added buttons and boxes to show the changes
    Also ran tests for different routes by adding buttons to guide to the test page and home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello world!</Button>
+    <main className="flex justify-center items-center min-h-dvh">
+      <Button asChild>
+        <Link href="/test">Test Page</Link>
+      </Button>
     </main>
   );
 }
