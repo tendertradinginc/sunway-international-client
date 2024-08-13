ee0831ed9fb54cea14924c567c7c2c289e6f1011
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-13 13:59:01 +0600

    exploring next js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Hello world!</Button>
+      <h1>Exploring ShadCn Ui with NEXT JS </h1>
     </main>
   );
 }
