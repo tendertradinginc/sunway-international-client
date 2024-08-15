f12b1510fa032a587838ca9526cb8902bac3bae3
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-15 14:17:35 +0600

    put new-arrival after banner

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
-      <NewArrival />
       <Hero />
+      <NewArrival />
       <Footer />
     </main>
   );
