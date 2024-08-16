91d76afb9fa0452ad912310fd9b48b3c9776329e
frustrated018 <niloyd018@gmail.com>
2024-08-16 18:15:46 +0600

    Fixed some responsive issues

--- src/components/Home/ProjectShowcase/ProjectShowcase.jsx
+++ src/components/Home/ProjectShowcase/ProjectShowcase.jsx
@@ -4,28 +4,28 @@ import { Button } from "@/components/ui/button";
 const projects = [
   {
     id: 1,
-    title: "Advanced Medical Center",
-    description: "Hospital Construction",
-    location: "Dhanmondi, Dhaka",
+    title: "Private Clinic",
+    description: "Healthcare Facility",
+    location: "Banani, Dhaka",
     imageUrl: "/adhy-savala-zbpgmGe27p8-unsplash.jpg",
   },
   {
     id: 2,
-    title: "Corporate Office Solutions",
+    title: "Office Solutions",
     description: "Office Equipment Supply",
     location: "Gulshan, Dhaka",
     imageUrl: "/benjamin-child-0sT9YhNgSEs-unsplash.jpg",
   },
   {
     id: 3,
-    title: "Luxury Private Clinic",
-    description: "Hospital Construction",
-    location: "Banani, Dhaka",
+    title: "Medical Center",
+    description: "Hospital Development",
+    location: "Dhanmondi, Dhaka",
     imageUrl: "/graham-ruttan-aMNLYoT2z_I-unsplash.jpg",
   },
   {
     id: 4,
-    title: "Commercial Medical Office",
+    title: "Medical Office",
     description: "Medical Equipment Supply",
     location: "Uttara, Dhaka",
     imageUrl: "/national-cancer-institute-oCLuFi9GYNA-unsplash.jpg",
@@ -41,7 +41,7 @@ const ProjectShowcase = () => {
         </h1>
       </MaxWidthWrapper>
 
-      <div className="relative min-h-screen w-full">
+      <div className="relative min-h-[600px] w-full">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{
@@ -51,29 +51,30 @@ const ProjectShowcase = () => {
           <MaxWidthWrapper className="w-full h-full">
             <section className="grid grid-cols-2 lg:gap-0 lg:grid-cols-4 h-full w-full p-[5%]">
               {projects.map((project) => (
-                <div key={project.id} className="bg-green-500 h-full w-full">
-                  <div className="relative w-full h-full">
-                    <div
-                      className="absolute inset-0 bg-cover bg-center"
-                      style={{
-                        backgroundImage: `url(${project.imageUrl})`,
-                      }}
-                    >
-                      {/* Black overlay */}
-                      <div className="absolute inset-0 bg-black opacity-40"></div>
-                      <section className="relative p-6 pt-10 text-muted">
-                        <div>
-                          <p className="text-base">{project.description}</p>
-                          <h4 className="text-xl font-medium">
-                            {project.title}
-                          </h4>
-                        </div>
-                        <h6 className="text-sm mt-3 mb-10">
-                          {project.location}
-                        </h6>
-                        <Button>View More</Button>
-                      </section>
-                    </div>
+                <div
+                  key={project.id}
+                  className="bg-green-500 relative h-full w-full"
+                >
+                  <div
+                    className="absolute inset-0 bg-cover bg-center"
+                    style={{
+                      backgroundImage: `url(${project.imageUrl})`,
+                    }}
+                  >
+                    {/* Black overlay */}
+                    <div className="absolute inset-0 bg-black opacity-40"></div>
+                    <section className="relative flex flex-col justify-evenly p-6 pt-10 text-muted h-full lg:h-1/2">
+                      <div className="space-y-1">
+                        <p className="text-sm md:text-base">{project.title}</p>
+                        <h4 className="text-base md:text-lg xl:text-xl font-medium">
+                          {project.description}
+                        </h4>
+                      </div>
+                      <h6 className="text-xs md:text-sm">
+                        {project.location}
+                      </h6>
+                      <Button className='w-full md:w-1/2'>View More</Button>
+                    </section>
                   </div>
                 </div>
               ))}
