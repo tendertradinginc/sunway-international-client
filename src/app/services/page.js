0c6521c5bd0d31519bce1600cb9c1c1ae5ddfd22
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-17 18:28:34 +0600

    added medical equipment section in services page

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,10 +1,12 @@
 import ContructionService from "@/components/services/ContructionService";
+import MedicalInstrument from "@/components/services/MedicalInstrument";
 import RegularEquipment from "@/components/services/RegularEquipment";
 
 const page = () => {
   return (
     <div className="py-10">
       <ContructionService />
+      <MedicalInstrument />
       <RegularEquipment />
     </div>
   );
