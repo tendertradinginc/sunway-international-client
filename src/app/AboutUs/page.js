16ea47a474518dce79b4584e025b01d5cfb1fa3b
husneara <husnearaa99@gmail.com>
2024-08-17 13:06:36 +0600

    has created the about us page component

---
+++ src/app/AboutUs/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const AboutUs = () => {
+    return (
+        <div>
+            <div>
+                <Image
+                    src="https://i.postimg.cc/htjzCHR7/aboutus.png"
+                    alt="About Us Image"
+                    width={500}
+                    height={300}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
