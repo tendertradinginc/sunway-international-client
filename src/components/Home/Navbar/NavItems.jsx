01aa6880f9e6caf4420a69ecc7ffa34647ac6f1b
sakil-sagor <sakilsagor76543@gmail.com>
2024-08-17 22:24:20 +0600

    fix

---
+++ src/components/Home/Navbar/NavItems.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "../../ui/button";
+
+const NavItems = ({ className }) => {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { id: 1, url: "/test", label: "Test Page" },
+    { id: 2, url: "#", label: "About Us" },
+    { id: 3, url: "#", label: "Gallery" },
+    { id: 4, url: "#", label: "Services" },
+    { id: 5, url: "#", label: "Blog" },
+    { id: 6, url: "#", label: "Products" },
+  ];
+
+  return (
+    <div className={cn("flex items-center", className)}>
+      {navLinks.map((link) => (
+        <Button
+          asChild
+          variant="link"
+          key={link.id}
+          className={cn(pathname === link.url ? "underline" : "")}
+        >
+          <Link href={link.url}>{link.label}</Link>
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default NavItems;
