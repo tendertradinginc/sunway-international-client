226444e11c20014b855fe79d493bbc7746a81c33
husneara <husnearaa99@gmail.com>
2024-08-17 16:07:17 +0600

    has completed the about CEo section

--- src/app/AboutUs/page.js
+++ src/app/AboutUs/page.js
@@ -17,42 +17,79 @@ const AboutUs = () => {
                     <h1 className="text-3xl md:text-5xl font-semibold text-si-primary py-8">
                         About Us
                     </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque magnam iure odit rerum minus maxime laboriosam asperiores ipsam reprehenderit, debitis deserunt quae. Adipisci, suscipit praesentium! Velit, placeat hic? Numquam consequuntur praesentium nihil tempore, dolore possimus assumenda aut ullam, corrupti voluptatem molestiae, ut accusantium! Maiores consectetur accusamus dolor molestiae magnam, nesciunt voluptatem eos, unde ea error dolore, fugit impedit cum! Reprehenderit nam, architecto ipsa nostrum aspernatur porro facilis, at eligendi in, sint aliquid nesciunt optio fuga aliquam a libero labore placeat! Quae excepturi expedita placeat at hic, aliquid maiores necessitatibus, a dolorem quo magnam error laudantium perferendis similique, doloribus sint rem? Ex esse dolorem consectetur molestias laboriosam corporis excepturi totam voluptate ipsum optio consequuntur expedita ad, dignissimos accusamus, maxime in recusandae suscipit inventore placeat asperiores cum. Dolore sed vitae provident et aspernatur error ipsam sit, rem autem obcaecati beatae nisi. Quis eius repudiandae libero enim odio dignissimos voluptatum atque debitis et minima ipsum ratione molestias ut totam, at voluptates. Minima, quasi quaerat cum sint dolorem aut reprehenderit necessitatibus maiores temporibus aliquid iusto recusandae tempore dolores repudiandae fuga quos a itaque cumque, quo ducimus, ullam blanditiis soluta? Maxime accusantium unde nobis eveniet iste voluptates illo fugit aut sed quibusdam quae, ut iusto id atque in ducimus. Repellendus, rem fugiat labore qui autem cumque amet animi numquam quisquam optio! Expedita inventore optio fuga, minima culpa sit! Earum, quod facilis. Voluptatem voluptas repellendus ducimus suscipit accusamus odit commodi temporibus corporis dolore, veniam totam sapiente, iste facere. Tenetur harum sint nesciunt deleniti saepe suscipit, ea asperiores facere laboriosam illo quaerat, iusto maxime cupiditate dolore reprehenderit quasi et nisi ab facilis maiores exercitationem? Minus provident, exercitationem eos culpa non aperiam expedita vero, qui, minima tempore aspernatur quas accusantium dignissimos voluptate neque eum explicabo reiciendis facilis aliquam ipsum modi quis. Laudantium inventore odio amet corrupti, asperiores molestias!
+                    <p className="text-justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque magnam iure odit rerum minus maxime laboriosam asperiores ipsam reprehenderit, debitis deserunt quae. Adipisci, suscipit praesentium! Velit, placeat hic? Numquam consequuntur praesentium nihil tempore, dolore possimus assumenda aut ullam, corrupti voluptatem molestiae, ut accusantium! Maiores consectetur accusamus dolor molestiae magnam, nesciunt voluptatem eos, unde ea error dolore, fugit impedit cum! Reprehenderit nam, architecto ipsa nostrum aspernatur porro facilis, at eligendi in, sint aliquid nesciunt optio fuga aliquam a libero labore placeat! Quae excepturi expedita placeat at hic, aliquid maiores necessitatibus, a dolorem quo magnam error laudantium perferendis similique, doloribus sint rem? Ex esse temporibus corporis dolore, veniam totam sapiente, iste facere. Tenetur harum sint nesciunt deleniti saepe suscipit, ea asperiores facere laboriosam illo quaerat, iusto maxime cupiditate dolore reprehenderit quasi et nisi ab facilis maiores exercitationem? Minus provident, exercitationem eos culpa non aperiam expedita vero, qui, minima tempore aspernatur quas accusantium dignissimos voluptate neque eum explicabo reiciendis facilis aliquam ipsum modi quis. Laudantium inventore odio amet corrupti, asperiores molestias!
                     </p>
-                    <section className="my-14 flex justify-between">
-                        <div className="flex-1 p-8">
+                </MaxWidthWrapper>
+                {/* vision section */}
+                <section className="my-10">
+                <h1 className="text-3xl md:text-5xl font-semibold text-si-primary py-8 px-4">
+                        About Vision
+                    </h1>
+                    <div className=" flex flex-col lg:flex-row justify-between gap-5 lg:gap-10 bg-secondary">
+                        <div className="flex-1 pt-3 text-justify px-4">
                             <h1 className="text-xl md:text-2xl font-semibold text-si-primary">
-                                MISSION
+                                Mission
                             </h1>
-                            <p>
+                            <p className="mt-2">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium rerum nihil est quidem cumque! Mollitia illo aliquam error odio, voluptatibus distinctio fugiat. Dicta facilis perferendis repudiandae!
                             </p>
-                            <h1 className="text-xl md:text-2xl font-semibold text-si-primary pt-4 ">
-                                VISION
+                            <h1 className="text-xl md:text-2xl font-semibold text-si-primary pt-4">
+                                Vision
                             </h1>
-                            <p>
+                            <p className="mt-2">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium rerum nihil est quidem cumque! Mollitia illo aliquam error odio, distinctio fugiat. Dicta facilis perferendis repudiandae!
                             </p>
-                            <h1 className="text-xxl md:text-2xl font-semibold text-si-primary pt-4">
-                                VALUS
+                            <h1 className="text-xl md:text-2xl font-semibold text-si-primary pt-4">
+                                Values
                             </h1>
-                            <p>
+                            <p className="mt-2 ">
                                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium rerum nihil est quidem cumque! Mollitia illo odio. Dicta facilis perferendis repudiandae!
                             </p>
                         </div>
 
-                        <div className="flex-1">
+                        <div className="flex-1 order-first lg:order-last">
                             <Image
                                 src="https://i.postimg.cc/s2XsprSv/sec-1.png"
                                 alt="About Us Image"
                                 width={500}
                                 height={300}
-                                className="w-full h-full"
+                                className="w-full h-auto object-cover pr-4"
                             />
                         </div>
-                    </section>
-                </MaxWidthWrapper>
+                    </div>
+                </section>
+
+
+                {/* about CEO section */}
+                <section className="mb-10">
+                    <h1 className="text-3xl md:text-5xl font-semibold text-si-primary py-8 px-4">
+                        About CEO
+                    </h1>
+                    <div className=" flex flex-col lg:flex-row justify-between gap-5 lg:gap-10 px-4 bg-secondary">
+
+                        <div className="flex-1 flex justify-center text-justify ">
+                            <Image
+                                src="https://i.postimg.cc/Z56VVB8Q/ceo.jpg"
+                                alt="About Us Image"
+                                width={500}
+                                height={300}
+                                className=" w-full h-auto object-cover rounded-none "
+                            />
+                        </div>
+
+                        <div className="flex-1 order-first lg:order-last text-justify ">
+                            <h1 className="text-xl md:text-2xl font-semibold text-si-primary pt-3">
+                                Engr Sarwar Sumon
+                            </h1>
+                            <h2 className="font-medium ">CEO At Sunway International.</h2>
+                            <p className="mt-4">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium rerum nihil est quidem cumque! Mollitia illo aliquam error odio,porro facilis, at eligendi in, sint aliquid nesciunt optio fuga aliquam a libero labore placeat! Quae excepturi expedita placeat at hic, aliquid maiores necessitatibus, a dolorem quo magnam error laudantium perferendis similique, doloribus sint rem? Ex esse temporibus corporis dolore, veniam totam sapiente, iste facere. Tenetur harum sint nesciunt deleniti saepe suscipit, ea asperiores facere laboriosam illo quaerat, iusto maxime cupiditate dolore reprehenderit quasi et nisi ab facilis maiores exercitationem? Minus provident, exercitationem eos culpa voluptatibus distinctio fugiat. Dicta facilis perferendis repudiandae!
+                            </p>
+                        </div>
+
+                    </div>
+                </section>
             </div>
         </div>
     );
