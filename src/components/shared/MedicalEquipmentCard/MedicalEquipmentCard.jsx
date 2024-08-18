67ed758eb33837f9b2e33a25591140c3986c1799
husneara <husnearaa99@gmail.com>
2024-08-18 15:30:19 +0600

    has linked with page

--- src/components/shared/MedicalEquipmentCard/MedicalEquipmentCard.jsx
+++ src/components/shared/MedicalEquipmentCard/MedicalEquipmentCard.jsx
@@ -14,15 +14,17 @@ const MedicalEquipmentCard = () => {
                 className="w-96"
             />
             <h3 className="text-lg mt-4 hover:underline">
-                <b>Product Name:</b> ECG 
+                <b>Product Name:</b> ECG
             </h3>
             <p className="text-sm line-clamp-2">
                 <b>Short Description:</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse maiores eligendi inventore qui commodi! Quam?
             </p>
             {/* <Link href={"/"} className="text-si-accent pa font-bold">Explore More</Link> */}
-            <button className="bg-[#fbbe24d3] rounded-sm px-3 p-1 mt-2 font-semibold text-black">
-            Explore More
-            </button>
+            <Link href={"/"}>
+                <button className="bg-[#fbbe24d3] rounded-sm px-3 p-1 mt-2 font-semibold text-black">
+                    Explore More
+                </button>
+            </Link>
         </div>
     );
 };
