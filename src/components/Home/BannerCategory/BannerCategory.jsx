939c030b22282cc425dd66feb9434ca7259d0759
husneara <husnearaa99@gmail.com>
2024-08-18 18:20:07 +0600

    has updated design and code

--- src/components/Home/BannerCategory/BannerCategory.jsx
+++ src/components/Home/BannerCategory/BannerCategory.jsx
@@ -1,33 +1,43 @@
-
+import { MdBathtub, MdOutlineListAlt } from "react-icons/md";
+import { RiFirstAidKitFill } from "react-icons/ri";
+import { TbWheelchair } from "react-icons/tb";
+import { PiBedBold } from "react-icons/pi";
+import { MdAddShoppingCart } from "react-icons/md";
 
 const BannerCategory = () => {
     return (
         <section >
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 border-b mb-8 ">
 
                 {/* category 1 */}
-                <div className=" flex justify-center items-center h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  via-[#faca5083] to-[#FEF3D7] text-white">
-                    <h1 className=" text-center text-black text-lg font-semibold">Walking Aids Series</h1>
+                <div className=" flex justify-center items-center flex-col h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
+                <RiFirstAidKitFill  className="text-6xl text-si-primary mb-2" />
+                    <h1 className=" text-center text-black text-md font-semibold">Walking Aids Series</h1>
                 </div>
                 {/* category 2 */}
-                <div className="flex justify-center items-center h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  via-[#faca5083] to-[#FEF3D7] text-white">
-                    <h1 className="text-center text-black text-lg font-semibold">Bathroom Safety Series</h1>
+                <div className="flex justify-center items-center flex-col h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
+                <MdBathtub className="text-6xl text-si-primary mb-2" />
+                    <h1 className="text-center text-black text-md font-semibold">Bathroom Safety Series</h1>
                 </div>
                 {/* category 3 */}
-                <div className="flex justify-center items-center h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  via-[#faca5083] to-[#FEF3D7] text-white">
-                    <h1 className="text-center text-black text-lg font-semibold">Wheelchair Series</h1>
+                <div className="flex justify-center items-center flex-col h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
+                <TbWheelchair className="text-6xl text-si-primary mb-2" />
+                    <h1 className="text-center text-black text-md font-semibold">Wheelchair Series</h1>
                 </div>
                 {/* category 4 */}
-                <div className="flex justify-center items-center h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  via-[#faca5083] to-[#FEF3D7] text-white">
-                    <h1 className="text-center text-black text-lg font-semibold">Hospital Bed Series</h1>
+                <div className="flex justify-center items-center flex-col h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
+                <PiBedBold  className="text-6xl text-si-primary mb-2" />
+                    <h1 className="text-center text-black text-md font-semibold">Hospital Bed Series</h1>
                 </div >
                 {/* category 5 */}
-                <div className="flex justify-center items-center h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  via-[#faca5083] to-[#FEF3D7] text-white">
-                    <h1 className="text-center text-black text-lg font-semibold">Hospital Furniture Series</h1>
+                <div className="flex justify-center items-center flex-col h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
+                <MdAddShoppingCart className="text-6xl text-si-primary mb-2" />
+                    <h1 className="text-center text-black text-md font-semibold">Hospital Furniture Series</h1>
                 </div>
                 {/* category 6 */}
-                <div className="flex justify-center items-center h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  via-[#faca5083] to-[#FEF3D7] text-white">
-                    <h1 className="text-center text-black text-lg font-semibold">Accessories</h1>
+                <div className="flex justify-center items-center flex-col h-32 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white">
+                <MdOutlineListAlt className="text-6xl text-si-primary mb-2" />
+                    <h1 className="text-center text-black text-md font-semibold">Accessories</h1>
                 </div>
 
             </div>
