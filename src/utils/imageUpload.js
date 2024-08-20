a40ce80bccdcb3f9e83d4bd2f8dc1cc7e87a37f3
sakil-sagor <sakilsagor76543@gmail.com>
2024-08-20 14:28:44 +0600

    create image upload fucntion

---
+++ src/utils/imageUpload.js
@@ -0,0 +1,46 @@
+const uploadImageToImgBB = async (imageFile) => {
+  if (!imageFile) {
+    return;
+  }
+  const apiKey = "c74cc0f2c97da68210a8d24aedf0b939";
+  const formData = new FormData();
+  formData.append("image", imageFile);
+
+  try {
+    const response = await fetch(
+      "https://api.imgbb.com/1/upload?key=" + apiKey,
+      {
+        method: "POST",
+        body: formData,
+      },
+    );
+
+    const data = await response.json();
+    if (data.status === 200) {
+      const imageUrl = data.data.url;
+      return imageUrl;
+    } else {
+      console.error("Image upload failed");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error uploading image: ", error);
+    return null;
+  }
+};
+
+export { uploadImageToImgBB };
+
+// use system
+
+//1
+// const [imageFile, setImageFile] = useState(null);
+
+//2
+// const handleImageUpload = (e) => {
+//   const file = e.target.files[0];
+//   setImageFile(file);
+// };
+
+//3
+// const imageUrl = await uploadImageToImgBB(imageFile);
