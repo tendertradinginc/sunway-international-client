d9f377524ad082ff4ff3c78b1406400174d0a476
husneara <husnearaa99@gmail.com>
2024-08-20 15:24:49 +0600

    has updated the all componenet

--- src/components/Dashboard/Dashboard/TotalConstractionBlogs.jsx
+++ src/components/Dashboard/Dashboard/TotalConstractionBlogs.jsx
@@ -6,7 +6,11 @@ const TotalConstructionBlogs = () => {
         <div>
             <div className="flex justify-center items-center gap-3 h-36 duration-500 bg-gradient-to-r from-[#16a249]  to-[#fbbf24] text-white">
                 <FaBloggerB className="text-6xl text-si-white mb-2" />
-                <h1 className=" text-center text-black text-md font-semibold">Total Blogs</h1>
+               
+                <div className="">
+                <h1 className=" text-center text-black text-base font-semibold">Blogs</h1>
+            <p className="flex justify-center items-center text-center text-white text-base font-semibold">50</p>
+            </div>
             </div>
         </div>
     );
--- src/components/Dashboard/Dashboard/TotalFurnitureEquipments.jsx
+++ src/components/Dashboard/Dashboard/TotalFurnitureEquipments.jsx
@@ -6,7 +6,10 @@ const TotalFurnitureEquipments = () => {
         <div>
             <div className="flex justify-center items-center gap-3 h-36 duration-500 bg-gradient-to-r from-[#16a249]  to-[#fbbf24] text-white">
                 <GiSofa className="text-7xl text-si-white mb-2" />
-                <h1 className=" text-center text-black text-md font-semibold">Total Blogs</h1>
+                <div className="">
+                <h1 className=" text-center text-black text-base font-semibold">Furniture Equipment</h1> 
+                    <p className="flex justify-center items-center text-center text-white text-base font-semibold">500</p>
+                </div>
             </div>
         </div>
     );
