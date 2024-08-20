66174ae33af8491b11977de5cbd8a47411ddf869
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-20 18:16:07 +0600

    added blog view functionality in dashboard

--- src/components/Dashboard/Blog/SingleBlog.jsx
+++ src/components/Dashboard/Blog/SingleBlog.jsx
@@ -3,23 +3,24 @@ import { CiEdit } from "react-icons/ci";
 import { MdDeleteOutline } from "react-icons/md";
 import BlogUpdate from "./BlogUpdate";
 import DeleteBlog from "./DeleteBlog";
+import ViewBlog from "./ViewBlog";
 
+const SingleBlog = ({ data, index }) => {
+  const { title } = data;
+  return (
+    <tr className={` ${index % 2 === 1 ? "bg-[#f2f2f2]" : ""}`}>
+      <td className="px-4 py-1">{index + 1}</td>
+      <td className="px-4 py-1 text-left font-semibold">{title}</td>
 
-const SingleBlog = ({ data, index}) => {
-    const { title } = data;
-    return (
-      <tr className={` ${index % 2 === 1 ? "bg-[#f2f2f2]" : ""}`}>
-        <td className="px-4 py-1">{index + 1}</td>
-        <td className="px-4 py-1  font-semibold text-left ">{title}</td>
-
-            <td className="md:px-4  space-x-3   ">
-                <AlertDialog className="flex gap-3">
-                        <BlogUpdate data={data} />
-                        <DeleteBlog id={data?._id}/>
-                </AlertDialog>
-            </td>
-        </tr>
-    );
+      <td className="space-x-3 md:px-4">
+        <AlertDialog className="flex gap-3">
+          <ViewBlog data={data}/>
+          <BlogUpdate data={data} />
+          <DeleteBlog id={data?._id} />
+        </AlertDialog>
+      </td>
+    </tr>
+  );
 };
 
 export default SingleBlog;
