9ff220957ab01b8a9cb8dd1762c466deb2265fb9
frustrated018 <niloyd018@gmail.com>
2024-08-21 20:16:25 +0600

    Basic setup for office equipment table
    Fixed the sidenav button linking issues with a links array and mapping over them
    Setup the tables page and column

---
+++ src/app/(withDashboardLayout)/dashboard/office-equipment-table/page.jsx
@@ -0,0 +1,21 @@
+import MaxWidthWrapper from "@/components/custom/MaxWidthWrapper";
+
+const officeEquipmentTablePage = async () => {
+  const data = await fetch(
+    "http://localhost:5000/api/v1/officeEquipment/all?search=&limit=&page=",
+  ).then((res) => res.json());
+
+  const products = data?.data?.result || [];
+
+  console.log(products[0]);
+
+  return (
+    <div>
+      <MaxWidthWrapper>
+        <h3>Total Office Equipment: 10</h3>
+      </MaxWidthWrapper>
+    </div>
+  );
+};
+
+export default officeEquipmentTablePage;
