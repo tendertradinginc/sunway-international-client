52809c3b3505b7bf36082aa8271ae4c3e5b79f23
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-21 20:06:14 +0600

    added skeleton in blogs and made pagination shared component

--- src/components/Dashboard/Blog/BlogTable.jsx
+++ src/components/Dashboard/Blog/BlogTable.jsx
@@ -2,13 +2,15 @@
 import { AiFillDatabase } from "react-icons/ai";
 import SingleBlog from "./SingleBlog";
 import BlogCreateButton from "./BlogCreateButton";
-import PaginationBlog from "./Pagination";
+import PaginationBlog from "../../shared/pagination/PaginationShadcn";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
 
 
 const BlogTable =  () => {
 const searchParams = useSearchParams()
+const [loading, setLoading] = useState(true)
 const [blogs, setBlogs] = useState([])
 const [page, setPage] = useState(searchParams.get('page' || 1))
 const [limit, setLimit] = useState(searchParams.get('limit' || 10))
@@ -21,7 +23,10 @@ useState(()=>{
   .then(data => {
     setBlogs(data?.data?.allBlogs)
     setTolatBlog(data?.data?.totlaBlogs)
+    setLoading(false)
+ 
   })
+  .catch(err => toast.error(err.message))
 },[page, limit])
 
 const totalPage = Math.ceil(totalBlog / limit)
@@ -50,7 +55,7 @@ console.log(totalPage)
                 </div>
               )} */}
 
-              {
+              { 
                 <table className="w-full table-auto">
                   <thead className="border-2 border-si-primary bg-si-primary text-white">
                     <tr className="">
@@ -61,20 +66,22 @@ console.log(totalPage)
                   </thead>
 
                   <tbody className="border text-center">
-                    {blogs?.map((blog, index) => (
+                    {blogs?.length > 0 ?  blogs?.map((blog, index) => (
                       <SingleBlog
                         key={index}
                         index={index}
                         data={blog}
                       ></SingleBlog>
-                    ))}
+                    )) : Array.from({ length: 10 }).map((_, idx)=><tr  key={idx} className={`h-10 w-full ${idx%2==0 ? "bg-secondary":""} `}>
+                      <td className="col" colSpan={4}></td>
+                    </tr>)}
                   </tbody>
                 </table>
               }
             </div>
-            <div className="mt-5">
+           {!loading && <div className="mt-5">
             <PaginationBlog data={{setPage, page, limit, totalPage}} />
-            </div>
+            </div>}
           </div>
         </div>
       </div>
