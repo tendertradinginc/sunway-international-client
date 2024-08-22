cbaee3d3b09979004fd0fb4a9dc14beb3c2042e7
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-22 18:13:27 +0600

    show category wise medical equipment card

--- src/components/custom/MedicalCategoryCard.jsx
+++ src/components/custom/MedicalCategoryCard.jsx
@@ -10,92 +10,74 @@ const MedicalCategoryCard = () => {
   // TODO: replace the links with the actual category
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/v1/medicalEquipment/all-category")
+    fetch("http://localhost:5000/api/v1/medicalEquipment/all-category", {
+      cache: "no-store",
+    })
       .then((res) => res.json())
       .then((data) => {
-        setLoading(true);
-       setData(data.data)
-       const test = data?.data?.map((item, idx)=> {
-        const categoryName = Object.keys(item)[0];
-        return categoryName
-        setCategories(categoryName)
-       })
-      console.log(test)
+        setLoading(false);
+        setData(data.data);
+        console.log(data?.data);
+        const test = data?.data?.map((item, idx) => {
+          const categoryName = Object.keys(item)[0];
+          setCategories(categoryName);
+          return categoryName;
+        });
       })
       .catch((error) => console.log(error));
-  }, [loading]);
+  }, []);
+
+
 
   return (
-    <MaxWidthWrapper className="py-16">
-       {data.map((categoryObj, index) => {
+    <MaxWidthWrapper className="py-16 min-h-[50vh]">
+       {data?.slice(0,4).map((categoryObj, index) => {
         // Access the keys of each object (category name)
         const categoryName = Object.keys(categoryObj)[0];
         const items = categoryObj[categoryName];
-
+        
         return (
-          <div key={index}>
-            <h2>{categoryName}</h2>
-            <div className="card-container">
-              {items.map((item, idx) => (
-                <Card key={idx} className="w-full mb-16">
-                <CardHeader>
-                  <div className="flex items-center justify-between border-b-2 pb-1">
-                    <CardTitle className="text-xl md:text-2xl lg:text-4xl">
-                      Linen
-                    </CardTitle>
-                    <Button asChild>
-                      <Link href={`/medical-equipment/${"linen"}`}>
-                        View More <DoubleArrowRightIcon className="ml-2 size-5" />
-                      </Link>
-                    </Button>
-                  </div>
-                </CardHeader>
-                <CardContent className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-                  {Array.from({ length: 4 }).map((_, index) => {
-                    return (
-                      <div key={index}>
-                        <EqipmentCard
-                          navigateTo={`/medical-equipment/linen/${index}`}
-                        />
-                      </div>
-                    );
-                  })}
-                </CardContent>
-              </Card>
-              ))}
+          <Card key={index} className="w-full mb-16">
+          <CardHeader>
+            <div className="flex items-center justify-between border-b-2 pb-1">
+              <CardTitle className="text-xl md:text-2xl lg:text-4xl">
+                {categoryName}
+              </CardTitle>
+              <Button asChild>
+                <Link href={`/medical-equipment/${"linen"}`}>
+                  View More <DoubleArrowRightIcon className="ml-2 size-5" />
+                </Link>
+              </Button>
             </div>
-          </div>
-        );
+          </CardHeader>
+          <CardContent className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            {items.map((item, index) => {
+              return (
+                <div key={index}>
+                  <EqipmentCard data={item}  navigateTo={`/medical-equipment/linen/${index}`} />
+                </div>
+              );
+            })}
+          </CardContent>
+        </Card>
+          // <div key={index}>
+          //   <h2>{categoryName}</h2>
+          //   <div className="card-container">
+          //     {items.map((item, idx) => (
+          //       <div key={idx} className="card">
+          //         {console.log(item)
+          //         }
+          //     <p>{item.productName}</p>
+          //       </div>
+          //     ))}
+          //   </div>
+          // </div>
+        )
       })}
-      {/* {data?.map((item, idx)=> <Card key={idx} className="w-full mb-16">
-        <CardHeader>
-          <div className="flex items-center justify-between border-b-2 pb-1">
-            <CardTitle className="text-xl md:text-2xl lg:text-4xl">
-              Linen
-            </CardTitle>
-            <Button asChild>
-              <Link href={`/medical-equipment/${"linen"}`}>
-                View More <DoubleArrowRightIcon className="ml-2 size-5" />
-              </Link>
-            </Button>
-          </div>
-        </CardHeader>
-        <CardContent className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          {Array.from({ length: 4 }).map((_, index) => {
-            return (
-              <div key={index}>
-                <EqipmentCard
-                  navigateTo={`/medical-equipment/linen/${index}`}
-                />
-              </div>
-            );
-          })}
-        </CardContent>
-      </Card>)} */}
- 
+      ;
     </MaxWidthWrapper>
   );
 };
