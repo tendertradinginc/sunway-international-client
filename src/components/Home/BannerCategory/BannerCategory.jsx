c746e06915601c7a7d1f621f99522b96e595db7c
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-24 17:31:22 +0600

    changed banner-category animation

--- src/components/Home/BannerCategory/BannerCategory.jsx
+++ src/components/Home/BannerCategory/BannerCategory.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { MdBathtub, MdOutlineListAlt } from "react-icons/md";
 import { RiFirstAidKitFill } from "react-icons/ri";
 import { PiBedBold } from "react-icons/pi";
@@ -7,44 +7,60 @@ import { MdAddShoppingCart } from "react-icons/md";
 import { motion } from "framer-motion";
 
 const BannerCategory = () => {
-    return (
-        <section className="overflow-x-hidden" >
-            <motion.div initial={{ x:1000 }} transition={{duration:1.2}} whileInView={{x:0}}  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 border-b mb-8 ">
+  return (
+    <section className="overflow-x-hidden">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        whileInView={{ opacity: 1, scale: 1 }}
 
-                {/* category 1 */}
-                <div className=" flex justify-center items-center flex-col h-36 duration-500 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
-                <RiFirstAidKitFill  className="text-6xl text-si-primary mb-2" />
-                    <h1 className=" text-center text-black text-md font-semibold">Walking Aids Series</h1>
-                </div>
-                {/* category 2 */}
-                <div className="flex justify-center items-center flex-col h-36 duration-500 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
-                <MdBathtub className="text-6xl text-si-primary mb-2" />
-                    <h1 className="text-center text-black text-md font-semibold">Bathroom Safety Series</h1>
-                </div>
-                {/* category 3 */}
-                <div className="flex justify-center items-center flex-col h-36 duration-500 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
-                <TfiWheelchair className="text-6xl text-si-primary mb-2" />
-                    <h1 className="text-center text-black text-md font-semibold">Wheelchair Series</h1>
-                </div>
-                {/* category 4 */}
-                <div className="flex justify-center items-center flex-col h-36 duration-500 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
-                <PiBedBold  className="text-6xl text-si-primary mb-2" />
-                    <h1 className="text-center text-black text-md font-semibold">Hospital Bed Series</h1>
-                </div >
-                {/* category 5 */}
-                <div className="flex justify-center items-center flex-col h-36 duration-500 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white border-r">
-                <MdAddShoppingCart className="text-6xl text-si-primary mb-2" />
-                    <h1 className="text-center text-black text-md font-semibold">Hospital Furniture Series</h1>
-                </div>
-                {/* category 6 */}
-                <div className="flex justify-center items-center flex-col h-36 duration-500 hover:bg-gradient-to-r from-[#fbbe24d3]  to-[#faca5083] text-white">
-                <MdOutlineListAlt className="text-6xl text-si-primary mb-2" />
-                    <h1 className="text-center text-black text-md font-semibold">Accessories</h1>
-                </div>
-
-            </motion.div>
-        </section>
-    );
+        transition={{ duration: 1.2 }}
+        className="mb-8 grid grid-cols-1 border-b md:grid-cols-2 lg:grid-cols-6"
+      >
+        {/* category 1 */}
+        <div className="flex h-36 flex-col items-center justify-center border-r from-[#fbbe24d3] to-[#faca5083] text-white duration-500 hover:bg-gradient-to-r">
+          <RiFirstAidKitFill className="mb-2 text-6xl text-si-primary" />
+          <h1 className="text-md text-center font-semibold text-black">
+            Walking Aids Series
+          </h1>
+        </div>
+        {/* category 2 */}
+        <div className="flex h-36 flex-col items-center justify-center border-r from-[#fbbe24d3] to-[#faca5083] text-white duration-500 hover:bg-gradient-to-r">
+          <MdBathtub className="mb-2 text-6xl text-si-primary" />
+          <h1 className="text-md text-center font-semibold text-black">
+            Bathroom Safety Series
+          </h1>
+        </div>
+        {/* category 3 */}
+        <div className="flex h-36 flex-col items-center justify-center border-r from-[#fbbe24d3] to-[#faca5083] text-white duration-500 hover:bg-gradient-to-r">
+          <TfiWheelchair className="mb-2 text-6xl text-si-primary" />
+          <h1 className="text-md text-center font-semibold text-black">
+            Wheelchair Series
+          </h1>
+        </div>
+        {/* category 4 */}
+        <div className="flex h-36 flex-col items-center justify-center border-r from-[#fbbe24d3] to-[#faca5083] text-white duration-500 hover:bg-gradient-to-r">
+          <PiBedBold className="mb-2 text-6xl text-si-primary" />
+          <h1 className="text-md text-center font-semibold text-black">
+            Hospital Bed Series
+          </h1>
+        </div>
+        {/* category 5 */}
+        <div className="flex h-36 flex-col items-center justify-center border-r from-[#fbbe24d3] to-[#faca5083] text-white duration-500 hover:bg-gradient-to-r">
+          <MdAddShoppingCart className="mb-2 text-6xl text-si-primary" />
+          <h1 className="text-md text-center font-semibold text-black">
+            Hospital Furniture Series
+          </h1>
+        </div>
+        {/* category 6 */}
+        <div className="flex h-36 flex-col items-center justify-center from-[#fbbe24d3] to-[#faca5083] text-white duration-500 hover:bg-gradient-to-r">
+          <MdOutlineListAlt className="mb-2 text-6xl text-si-primary" />
+          <h1 className="text-md text-center font-semibold text-black">
+            Accessories
+          </h1>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default BannerCategory;
