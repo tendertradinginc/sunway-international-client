22965e13b268fb4937edce7b5ac32c9f20778fae
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-25 17:10:38 +0600

    added animation in services section in home page

--- src/components/shared/ServiceCard/ServiceCard.js
+++ src/components/shared/ServiceCard/ServiceCard.js
@@ -1,13 +1,23 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
+import { motion } from "framer-motion";
 
-const ServiceCard = ({ data }) => {
+const ServiceCard = ({ data, index }) => {
   const [isShow, setIsShow] = useState(false);
+  const scrollRef = useRef(null);
   return (
-    <div className="rounded-md shadow-2xl border-2 border-t-0">
+    <motion.div
+      initial={{ y: 300 }}
+      whileInView={{  y: 0 }}
+      transition={{
+        duration: (index+1) * .5,
+        ease: [0, .71, .71, 1.01],
+      }}
+      className="rounded-md border-2 border-t-0 shadow-2xl"
+    >
       <div
         className="relative"
         onMouseOver={() => setIsShow(true)}
@@ -17,32 +27,34 @@ const ServiceCard = ({ data }) => {
           height={300}
           width={400}
           alt="construction"
-          className="w-full h-64"
+          className="h-64 w-full"
           src={data?.imageUrl}
         />
         <div
-          className={`absolute h-full flex flex-col justify-center w-full px-8  left-0 top-0 duration-500   ${
-           isShow ? "bg-[#22272798]" : ""
+          className={`absolute left-0 top-0 flex h-full w-full flex-col justify-center px-8 duration-500 ${
+            isShow ? "bg-[#22272798]" : ""
           }`}
         >
-          <div  className={` transition duration-500  ease-in-out ${
-            isShow ? "opacity-100" : "opacity-0 "
-          }`}>
-            <p className={`text-3xl text-si-accent `}>{data?.icon}</p>
-            <h1 className="text-white font-semibold text-2xl my-5">
+          <div
+            className={`transition duration-500 ease-in-out ${
+              isShow ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <p className={`text-3xl text-si-accent`}>{data?.icon}</p>
+            <h1 className="my-5 text-2xl font-semibold text-white">
               {data?.title}
             </h1>
             <Link className="text-si-accent" href={data?.url}>
               Read More &nbsp;{" "}
-              <FaArrowRight className="text-si-accent inline-block" />
+              <FaArrowRight className="inline-block text-si-accent" />
             </Link>
           </div>
         </div>
       </div>
-      <div className="p-8 text-si-text font-medium bg-secondary ">
+      <div className="bg-secondary p-8 font-medium text-si-text">
         <p>{data?.description}</p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
