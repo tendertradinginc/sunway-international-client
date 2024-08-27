f7c89bf23686a3717d7f4e53df6638a238ea727b
HalimaSaadia <halimasadia9666@gmail.com>
2024-08-27 14:49:16 +0600

    added FFU section in gallery page

---
+++ src/app/(withCommonLayout)/gallery/page.js
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "@/components/custom/MaxWidthWrapper";
+import FFU from "@/components/gallery/FFU";
+
+const page = () => {
+    return (
+        <MaxWidthWrapper className="py-16 space-y-20">
+            <FFU />
+
+            
+        </MaxWidthWrapper>
+    );
+};
+
+export default page;
---
+++ src/components/gallery/FFU.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+
+const FFU = () => {
+    return (
+        <div>
+            <h1 className="text-3xl md:text-5xl font-semibold text-si-primary">FFU Workshop</h1>
+            <Image className="mt-8" src="https://i.postimg.cc/PrDdZJSG/tunnel-dryer-1.png" alt="FFU Workshop Image" height={800} width={1400} />
+            
+        </div>
+    );
+};
+
+export default FFU;
