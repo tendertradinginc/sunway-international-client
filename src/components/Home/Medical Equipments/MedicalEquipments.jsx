14a354f5b82529e037d68145ec43138f7e160fb5
sakil-sagor <sakilsagor76543@gmail.com>
2024-09-01 16:46:24 +0600

    update

--- src/components/Home/Medical Equipments/MedicalEquipments.jsx
+++ src/components/Home/Medical Equipments/MedicalEquipments.jsx
@@ -12,7 +12,7 @@ const MedicalEquipments = async () => {
   return (
     <MaxWidthWrapper className="mt-20 overflow-x-hidden py-20">
       <h1 className="mb-12 text-3xl font-semibold text-si-primary md:text-5xl">
-        Medical Equipments
+        Medical <span className="text-[#fbbf24]"> Equipments</span>
       </h1>
       <div className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-4">
         {blogs.map((item, index) => {
--- src/components/Home/NewArrival/Services.js
+++ src/components/Home/NewArrival/Services.js
@@ -1,17 +1,14 @@
 import MaxWidthWrapper from "@/components/custom/MaxWidthWrapper";
 import ServiceCard from "@/components/shared/ServiceCard/ServiceCard";
-import { FaBuilding } from "react-icons/fa";
-import { FaStethoscope } from "react-icons/fa";
-import { FaWheelchair } from "react-icons/fa";
+import { FaBuilding, FaStethoscope, FaWheelchair } from "react-icons/fa";
 
 const Services = () => {
-
   return (
     <MaxWidthWrapper className="py-16">
-      <h1 className="text-3xl md:text-5xl font-semibold text-si-primary">
-        Our Services
+      <h1 className="mb-12 text-3xl font-semibold text-si-primary md:text-5xl">
+        Our <span className="text-[#fbbf24]"> Services</span>
       </h1>
-      <div className="grid overflow-hidden grid-cols-1 py-16 md:grid-cols-2 lg:grid-cols-3 gap-y-14 gap-x-10 ">
+      <div className="grid grid-cols-1 gap-x-10 gap-y-14 overflow-hidden py-16 md:grid-cols-2 lg:grid-cols-3">
         {data?.map((item, idx) => (
           <ServiceCard index={idx} key={idx} data={item} />
         ))}
@@ -33,7 +30,8 @@ const data = [
     description:
       " Contruction in the process of constructing a building or  infrastructure. Contruction differs from manufacturing typically involves mass production of similar items a designated purchaser.",
     url: "",
-    imageUrl:"https://www.bls.gov/spotlight/2022/the-construction-industry-labor-force-2003-to-2020/images/cover-image.jpg",
+    imageUrl:
+      "https://www.bls.gov/spotlight/2022/the-construction-industry-labor-force-2003-to-2020/images/cover-image.jpg",
   },
   {
     icon: <FaStethoscope />,
@@ -45,7 +43,8 @@ const data = [
     description:
       " Contruction in the process of constructing a building or  infrastructure. Contruction differs from manufacturing typically involves mass production of similar items a designated purchaser.",
     url: "",
-    imageUrl:"https://i.postimg.cc/6pXVVBS1/marcel-scholte-LPur-Jnihm-QI-unsplash.jpg",
+    imageUrl:
+      "https://i.postimg.cc/6pXVVBS1/marcel-scholte-LPur-Jnihm-QI-unsplash.jpg",
   },
   {
     icon: <FaWheelchair />,
@@ -57,6 +56,7 @@ const data = [
     description:
       " Contruction in the process of constructing a building or  infrastructure. Contruction differs from manufacturing typically involves mass production of similar items a designated purchaser.",
     url: "",
-    imageUrl:"https://i.postimg.cc/jddtDfq8/pexels-fotios-photos-1957478-2-1.jpg",
+    imageUrl:
+      "https://i.postimg.cc/jddtDfq8/pexels-fotios-photos-1957478-2-1.jpg",
   },
 ];
