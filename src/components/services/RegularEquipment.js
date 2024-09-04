64edb95e97e5cd7eb67a61ca2eb91e9712738096
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-04 21:42:15 +0600

    Updage hospita-furniture serivce content

--- src/components/services/RegularEquipment.js
+++ src/components/services/RegularEquipment.js
@@ -49,15 +49,10 @@ const RegularEquipment = () => {
 
         <div>
           <h1 className="text-3xl md:text-5xl font-semibold text-si-primary ">
-            Regular Equipment
+           Hospital Furniture
           </h1>
           <p className="text-justify mt-8 text-si-text font-medium ">
-            Here is a comprehensive description for the hospital construction
-            section: Comprehensive Hospital Construction Solutions At Sunrise
-            International, we specialize in delivering end-to-end hospital
-            construction services that meet the highest standards of healthcare
-            infrastructure. From initial planning and design to construction and
-            final handover, our experienced team ensures that every project is
+            Our Hospital Furniture Providing services are crucial for maintaining the functional and aesthetic quality of healthcare environments. These services provide a continuous supply of essential furniture items like hospital beds, chairs, tables, storage units, and more, ensuring that all areas, from patient rooms to waiting areas and administrative offices, are equipped to meet the demands of daily operations. The furniture provided is often designed to meet strict healthcare standards, prioritizing durability, hygiene, and ease of maintenance.
           </p>
           <ul className="mt-5 bg-opacity-20 p-3">
             <li className="flex items-center text-xl font-semibold py-3  border-gray-300 gap-3">
