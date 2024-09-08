ceb12bccf743af416fa09f59b2ebad8282a86ec7
frustrated018 <niloyd018@gmail.com>
2024-09-08 15:52:38 +0600

    Updated euqipment card
    Fixed the responsive issues of the cards not staying the same height depending on content

--- src/components/shared/EquipmentCard/EquipmentCard.jsx
+++ src/components/shared/EquipmentCard/EquipmentCard.jsx
@@ -11,7 +11,7 @@ const EquipmentCard = ({ navigateTo = "", data }) => {
       initial={{ opacity: 0, scale: 1.1 }}
       whileInView={{ opacity: 1, scale: 1 }}
       transition={{ duration: 1.2 }}
-      className="min-h-[400px] bg-background"
+      className="flex h-full min-h-[400px] flex-col bg-background"
     >
       <div className="relative mx-auto h-[250px] w-full">
         <Image
@@ -22,17 +22,24 @@ const EquipmentCard = ({ navigateTo = "", data }) => {
           loader={customLoader}
         />
       </div>
-      <div className="text-balance border-b px-2.5 py-3 text-left shadow-xl shadow-green-100">
-        <h3 className="line-clamp-1 text-lg font-bold">{data?.productName}</h3>
-        {/* <p className="mx-auto my-2 line-clamp-2 text-wrap text-sm text-muted-foreground">
-          {data?.shortDescription}
-        </p> */}
-        <p className="mb-4 mt-1 line-clamp-2 flex-grow text-sm">
-          <span>Model:</span> <b>{data?.model}</b>
-        </p>
-        <Button asChild variant="custom">
-          <Link href={navigateTo}>View Details</Link>
-        </Button>
+      <div className="h-full flex-1 border-b px-2.5 py-3 text-left shadow-xl shadow-green-100">
+        <div className="flex h-full flex-col items-start justify-between">
+          <div className="flex-1">
+            <h3 className="line-clamp-2 text-lg font-bold">
+              {data?.productName}
+            </h3>
+            <p className="line-clamp-2 flex-grow text-sm">
+              <span className="font-semibold">Model:</span>{" "}
+              {data && (data.modelNumber || data.model)}
+            </p>
+            <p className="mx-auto my-2 line-clamp-2 text-wrap text-sm text-muted-foreground">
+              {data?.shortDescription}
+            </p>
+          </div>
+          <Button asChild variant="custom">
+            <Link href={navigateTo}>View Details</Link>
+          </Button>
+        </div>
       </div>
     </motion.div>
   );
