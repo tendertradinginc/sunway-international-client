9c11fbffd2d9305873c9373f94367493b49089ea
husneara <husnearaa99@gmail.com>
2024-09-12 18:55:37 +0600

    has updated the managing partner section

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -65,101 +65,146 @@ const AboutUs = () => {
           </div>
         </section>
 
-
-        <hr />
         {/* vision section */}
-        <section className="my-16">
-          <div className="mx-auto max-w-6xl py-8">
-
-          </div>
-          <div className="mx-auto flex max-w-6xl flex-col justify-between gap-5 py-5 lg:flex-row lg:gap-10">
+        <section className=" bg-[#F6F6F6] pb-6">
+          <div className="mx-auto max-w-5xl py-5">
             <div className="flex-1 px-4 text-justify">
-              <h1 className="text-xl font-semibold text-si-primary md:text-2xl">
-                Mission
-              </h1>
-              <p className="mt-2">
-                Elevating standards with top-tier products and
-                services—universally embraced and accessible. Setting the
-                benchmark for excellence in affordability.
-              </p>
-              <h1 className="pt-4 text-xl font-semibold text-si-primary md:text-2xl">
-                Vision
-              </h1>
-              <p className="mt-2">
-                Striving to be the forefront authority in global healthcare
-                solutions – setting new standards in products and services.
-              </p>
-              <h1 className="pt-4 text-xl font-semibold text-si-primary md:text-2xl">
-                Strategy
-              </h1>
-              <p className="mt-2">
-                Striving for continuous improvement in product quality and
-                innovation through better services.
-              </p>
-              <h1 className="pt-4 text-xl font-semibold text-si-primary md:text-2xl">
-                Goals
-              </h1>
-              <p className="mt-2">
-                Designing, developing, and marketing proprietary products and
-                services employing, exploiting, and harnessing advanced and
-                state-of-the-art technological solutions.
-              </p>
-            </div>
 
-            <div className="order-first flex flex-1 justify-center text-justify lg:order-last">
-              <Image
-                src="https://i.ibb.co.com/xCDrgKj/hospitals.webp"
-                alt="About Us Image"
-                width={500}
-                height={300}
-                loader={customLoader}
-                className="h-auto w-full rounded-none object-cover"
-              />
+              <div className="flex flex-col lg:flex-row items-center gap-12 mt-12">
+                <div>
+                  <Image
+                    src="https://i.postimg.cc/GmN1ZVfL/Group.png"
+                    alt="mission"
+                    width={500}
+                    height={300}
+                    loader={customLoader}
+                    className="h-auto w-28 rounded-none"
+                  />
+                </div>
+                <div>
+                  <h1 className="text-xl font-semibold text-si-primary md:text-2xl">
+                    Our Mission
+                  </h1>
+                  <p className="mt-2">
+                    Elevating standards with top-tier products and
+                    services—universally embraced and accessible. Setting the
+                    benchmark for excellence in affordability.
+                  </p>
+                </div>
+              </div>
+
+
+              <div className="flex flex-col lg:flex-row items-center gap-12 mt-12">
+                <div>
+                  <Image
+                    src="https://i.postimg.cc/zvJmCryj/Group-2.png"
+                    alt="vision"
+                    width={500}
+                    height={300}
+                    loader={customLoader}
+                    className="h-auto w-24 rounded-none"
+                  />
+                </div>
+                <div>
+                  <h1 className="text-xl font-semibold text-si-primary md:text-2xl">
+                    Our Vision
+                  </h1>
+                  <p className="mt-2">
+                    Striving to be the forefront authority in global healthcare
+                    solutions – setting new standards in products and services.
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex flex-col lg:flex-row items-center gap-12 mt-12">
+                <div>
+                  <Image
+                    src="https://i.postimg.cc/FsR8NG37/Group-1.png"
+                    alt="goals"
+                    width={500}
+                    height={300}
+                    loader={customLoader}
+                    className="h-auto w-20 rounded-none"
+                  />
+                </div>
+                <div>
+                  <h1 className="text-xl font-semibold text-si-primary md:text-2xl">
+                    Our Strategy
+                  </h1>
+                  <p className="mt-2">
+                    Striving for continuous improvement in product quality and
+                    innovation through better services.
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex flex-col lg:flex-row items-center gap-12 mt-12">
+                <div>
+                  <Image
+                    src="https://i.postimg.cc/yNFrvG4r/Group-3.png"
+                    alt="goals"
+                    width={500}
+                    height={300}
+                    loader={customLoader}
+                    className="h-auto w-28 rounded-none"
+                  />
+                </div>
+                <div>
+                  <h1 className="text-xl font-semibold text-si-primary md:text-2xl">
+                    Our Goals
+                  </h1>
+                  <p className="mt-2">
+                    Designing, developing, and marketing proprietary products and
+                    services employing, exploiting, and harnessing advanced and
+                    state-of-the-art technological solutions.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
 
-        <hr />
-
         {/* about CEO section */}
         <section className="my-16">
-          <div className="mx-auto max-w-6xl py-8">
-            {/* <h1 className="text-3xl md:text-5xl font-semibold text-si-primary py-8 px-4">
-                            Managing Partner
-                        </h1> */}
-            <SectionTitle title1={"Managing"} title2={"Partner"}></SectionTitle>
-          </div>
           <div className="mx-auto flex max-w-6xl flex-col justify-between gap-5 px-4 py-5 lg:flex-row lg:gap-10">
-            <div className="flex flex-1 justify-center text-justify">
+            <div className="flex-1 text-justify">
               <Image
                 src="https://i.postimg.cc/26cJFk8r/Whats-App-Image-2024-09-07-at-4-33-35-PM.jpg"
                 alt="About Us Image"
                 width={500}
                 height={300}
-                className="h-auto w-96 rounded-full object-cover"
+                className="h-auto w-full rounded-none object-cover shadow-md"
               />
+              <div className="relative z-10 bg-white w-max -mt-8 mx-auto p-2">
+                <h1 className="text-xl font-semibold text-si-primary md:text-xl text-center">
+                  Mohammad A K Mozumder
+                </h1>
+                <h2 className="font-medium text-sm">Managing Partner At Twin Trade International</h2>
+              </div>
+
             </div>
 
+
             <div className="flex-1 text-justify">
-              <h1 className="pt-3 text-xl font-semibold text-si-primary md:text-2xl">
-                Mohammad Abul Kalam Mozumder
+              <h1 className=" text-xl font-bold text-si-primary md:text-3xl">
+                Managing Partner
               </h1>
-              <h2 className="font-medium">Managing Partner At Twin Trade International</h2>
+
               <p className="mt-4">
                 With a modest beginning in the trade and business in 2000, Twin
                 Trade International has transformed itself into a conglomerate
-                boasting 14 companies within its fold over time. These companies
+                boasting 14 companies within its fold over time. <br /> <br /> These companies
                 are now engaged in diverse areas of business, commerce, and
                 industries. Twin Trade International has been consistently
                 striving hard to become the market leader in hospital furniture,
                 medical devices, pharmaceutical, agro-business, veterinary,
-                fashion, furniture, engineering, and media industries. Indeed,
+                fashion, furniture, engineering, and media industries.<br /> <br />  Indeed,
                 in some of the areas, it acted as the pioneer and prime mover
                 and is recognized as the leading brand. It excelled in quality,
                 services, and commitment towards the stakeholders, and obtained
-                and secured ISO, CE, and FDA certifications for its concerns. It
+                and secured ISO, CE, and FDA certifications for its concerns. <br /><br /> It
                 tirelessly strived to equip a new breed of entrepreneurs in the
-                import substitution medical equipment and devices sector. It
+                import substitution medical equipment and devices sector.<br /><br />  It
                 played catalytic roles and worked with the government in
                 recognizing and including medical device manufacturing in the
                 National Industrial Policy 2022.
@@ -168,7 +213,6 @@ const AboutUs = () => {
           </div>
         </section>
 
-        <hr />
 
         {/* about Partner section */}
         <section className="my-16">
