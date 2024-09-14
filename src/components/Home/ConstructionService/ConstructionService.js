9cde7ea4b930fd8913c7c3c3a343a29c40733e29
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-14 19:29:26 +0600

    modify hero section in home page

--- src/components/Home/ConstructionService/ConstructionService.js
+++ src/components/Home/ConstructionService/ConstructionService.js
@@ -1,15 +1,19 @@
 "use client";
+import SectionTitle from "@/components/shared/SectionTitle/SectionTitle";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 import MaxWidthWrapper from "../../custom/MaxWidthWrapper";
+import "./construction.css";
 
 const ConstructionService = () => {
   return (
-    <MaxWidthWrapper className="overflow-x-hidden py-16 mt-10">
-      <h1 className="mb-2 text-3xl font-semibold text-si-primary md:text-5xl">
-        Hospital <span className="text-[#fbbf24]"> Renovation</span>
-      </h1>
-      <div className="mt-10 grid grid-cols-12 grid-rows-12 gap-y-10 md:gap-10">
+    <MaxWidthWrapper className="overflow-x-hidden py-24">
+      <SectionTitle
+        title="Hospital Renovation"
+        description="Twin Trade International aims to create spaces that heal, supply tools that cure as we deliver excellence to your doorstep"
+      />
+      <div className="gid bg-container mt-10">
         <motion.div
           initial={{ x: -500 }}
           whileInView={{ x: 0 }}
@@ -17,59 +21,31 @@ const ConstructionService = () => {
             duration: 1.8,
             ease: [0, 0.71, 0.2, 1.01],
           }}
-          className="col-span-12 row-span-12 bg-[url('https://i.ibb.co/XFvgv42/national-cancer-institute-w7-Pby5b-DKW4-unsplash-1.jpg')] bg-cover md:col-span-6 md:row-span-12"
+          className="relative bg-cover"
         >
-          <div className="flex h-full w-full flex-col justify-end gap-y-2 bg-[#0c0d0d4a] p-8 text-white">
-            <p className="mt-10">BUILDING</p>
-            <h1 className="text-3xl font-semibold">Modern Computerized</h1>
-            <p>Dhanmondi, Dhaka</p>
-            <Link href="/services">
-              {" "}
-              <button className="mt-8 rounded-md border-2 border-si-accent p-2 px-5 duration-300 hover:bg-si-accent">
-                View More
+          <div className="grid grid-cols-1 px-8 py-32 md:grid-cols-2">
+            <div className="text-[#EFEFEF]">
+              <h3 className="text-xl">BUILDING</h3>
+              <h2 className="my-3 text-3xl font-bold text-[#EFEFEF]">
+                Latest ICU Room For <br /> Emergency
+              </h2>
+              <p className="my-6">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard Lorem
+                Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard
+              </p>
+              <button className="flex items-center gap-x-2 rounded-md border border-transparent text-lg font-semibold duration-300 hover:border-[#fff] hover:text-[#fff]">
+                <Link className="px-3 py-2" href="/services">
+                  View more{" "}
+                </Link>
+                <span>
+                  {" "}
+                  <FaArrowRight />
+                </span>
               </button>
-            </Link>
-          </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ x: 500 }}
-          whileInView={{ x: 0 }}
-          transition={{
-            duration: 1.8,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
-          className="col-span-12 row-span-12 bg-[url('https://i.ibb.co/kV7bTKh/martha-dominguez-de-gouveia-KF-h9-HMx-RKg-unsplash-1.jpg')] bg-cover bg-center md:col-span-6 md:row-span-5"
-        >
-          <div className="flex h-full w-full flex-col justify-center gap-y-2 bg-gradient-to-r from-[#0c0d0de7] via-[#0c0d0db2] to-transparent p-5 pl-8 text-left text-white">
-            <p className="mt-5">GENERAL DIRECTOR</p>
-            <h1 className="text-3xl font-semibold">We are hiring specialist</h1>
-
-            <Link href="#">
-              {" "}
-              <button className="mt-5 rounded-lg border-2 border-si-accent bg-si-accent p-2 px-5 duration-300 hover:bg-transparent">
-                View More
-              </button>
-            </Link>
-          </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ x: 600 }}
-          whileInView={{ x: 0 }}
-          transition={{
-            duration: 2,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
-          className="col-span-12 row-span-12 bg-[url('https://i.ibb.co/8bMq9rs/national-cancer-institute-f-TQHPb6r4w-Q-unsplash.jpg')] bg-cover bg-center md:col-span-6 md:row-span-7"
-        >
-          <div className="flex h-full w-full flex-col items-end justify-center gap-y-2 bg-gradient-to-r from-[#0c0d0d00] via-[#0c0d0dee] to-transparent p-5 pl-8 text-right text-white">
-            <div>
-              <p className="mt-2 text-muted">Contruction</p>
-              <h1 className="text-3xl font-semibold">
-                Twin Trade Int. - Contact Us
-              </h1>
             </div>
+            <div></div>
           </div>
         </motion.div>
       </div>
--- src/components/shared/Navbar/NavItems.jsx
+++ src/components/shared/Navbar/NavItems.jsx
@@ -18,31 +18,24 @@ const NavItems = ({ className }) => {
   const pathname = usePathname();
 
   const navLinks = [
-    { id: 1, url: "/", label: "Home" },
-    { id: 2, url: "/AboutUs", label: "About Us" },
-    { id: 3, url: "/gallery", label: "Gallery" },
-    { id: 4, url: "/services", label: "Services" },
-    { id: 5, url: "/blog", label: "Blog" },
-    // { id: 6, url: "/dashboard", label: "Dashboard" },
+    { id: 1, url: "/services", label: "Services" },
+    { id: 2, url: "/gallery", label: "Gallery" },
+    { id: 3, url: "/AboutUs", label: "About Us" },
+    { id: 4, url: "/blog", label: "Blog" },
   ];
 
   return (
     <div className="flex flex-col items-center md:flex-row">
-      <div className="flex flex-col items-center md:flex-row">
-        {navLinks.map((link) => (
-          <Button
+            <Button
             asChild
             variant="link"
-            key={link.id}
-            className={cn(pathname === link.url ? "underline" : "")}
+         
+            className={cn(pathname === "/" ? "underline" : "")}
           >
-            <Link href={link.url} className="font-semibold">
-              {link.label}
+            <Link href={"/"} className="font-semibold">
+              Home
             </Link>
           </Button>
-        ))}
-      </div>
-
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
@@ -67,10 +60,27 @@ const NavItems = ({ className }) => {
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+      <div className="flex flex-col items-center md:flex-row">
+        {navLinks.map((link) => (
+          <Button
+            asChild
+            variant="link"
+            key={link.id}
+            className={cn(pathname === link.url ? "underline" : "")}
+          >
+            <Link href={link.url} className="font-semibold">
+              {link.label}
+            </Link>
+          </Button>
+        ))}
+      </div>
 
-      <div className="block md:hidden mt-2  ">
-        <Link href="/AboutUs#contact-section">  <Button>Contact Us</Button></Link>
-        </div>
+      <div className="mt-2 block md:hidden">
+        <Link href="/AboutUs#contact-section">
+          {" "}
+          <Button>Contact Us</Button>
+        </Link>
+      </div>
     </div>
   );
 };
