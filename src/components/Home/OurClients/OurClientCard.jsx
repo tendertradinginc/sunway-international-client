e54b47077b2b52384063416f8a9919281743ed63
husneara <husnearaa99@gmail.com>
2024-09-14 18:36:17 +0600

    has updated the client card

--- src/components/Home/OurClients/OurClientCard.jsx
+++ src/components/Home/OurClients/OurClientCard.jsx
@@ -5,12 +5,21 @@ const OurClientCard = ({ client }) => {
     const { img } = client;
     return (
         <div>
-            <div className=" flex items-center  p-6 gap-6  text-center rounded-lg shadow-xl hover:-translate-y-6 duration-700 cursor-pointer">
+            {/* <div className=" flex items-center  p-4 py-8 gap-6  text-center rounded-lg shadow-xl hover:-translate-y-6 duration-700 cursor-pointer">
                 <Image
                     width={500}
                     height={200}
-                    className="h-24 mx-auto"
+                    className="h-20 mx-auto"
                     src={img} alt="clients"
+                />
+            </div> */}
+            <div className="flex h-36 flex-col items-center justify-center rounded-lg shadow-xl  text-white  bg-white hover:-translate-y-6 duration-700 cursor-pointer">
+                <Image
+                    src={img}
+                    alt="clients"
+                    height="100"
+                    width="200"
+                    className=""
                 />
             </div>
         </div>
