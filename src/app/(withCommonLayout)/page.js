3630d0d9f7baa73d63a9984a985130ee46793b5f
sakil-sagor <sakilsagor76543@gmail.com>
2024-09-15 11:55:53 +0600

    update homepage

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -40,9 +40,9 @@ export default function Home() {
       <Testimonials />
 
       {/* <AuthorizeDistribution /> */}
-      <AuthorizeDistributors />
 
       <OurClients />
+      <AuthorizeDistributors />
 
       <Chatbot />
     </div>
