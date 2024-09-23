20472c05bc108f775527a4ff109c8b695ed01a54
sakil-sagor <sakilsagor76543@gmail.com>
2024-09-23 13:40:11 +0600

    update client

--- src/components/Home/OurClients/OurClientCard.jsx
+++ src/components/Home/OurClients/OurClientCard.jsx
@@ -3,16 +3,9 @@ import Image from "next/image";
 
 const OurClientCard = ({ client }) => {
   const { img } = client;
+  console.log(img);
   return (
     <div>
-      {/* <div className=" flex items-center  p-4 py-8 gap-6  text-center rounded-lg shadow-xl hover:-translate-y-6 duration-700 cursor-pointer">
-                <Image
-                    width={500}
-                    height={200}
-                    className="h-20 mx-auto"
-                    src={img} alt="clients"
-                />
-            </div> */}
       <div className="flex h-36 cursor-pointer flex-col items-center justify-center rounded-lg bg-white text-white shadow-xl duration-700 hover:-translate-y-6">
         <Image
           loader={customLoader}
