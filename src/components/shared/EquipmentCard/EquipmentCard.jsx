afbf1af4c73c61aa0a30371230f8a6e4a7fc7def
HalimaSaadia <halimasadia9666@gmail.com>
2024-09-28 12:51:41 +0600

    given fix height to the  equipment card

--- src/components/shared/EquipmentCard/EquipmentCard.jsx
+++ src/components/shared/EquipmentCard/EquipmentCard.jsx
@@ -8,13 +8,14 @@ const EquipmentCard = ({ navigateTo = "", data }) => {
   return (
     <Card className="flex h-full flex-col">
       <CardHeader className="flex-grow text-center">
+       
         <Image
           loader={customLoader}
           src={data.images[0]}
           alt={data.name}
           width={200}
           height={200}
-          className="mb-4 h-auto w-full object-contain"
+          className="mb-4  w-full object-contain h-60 "
         />
         <CardTitle className="break-words text-lg font-semibold">
           {data.productName}
