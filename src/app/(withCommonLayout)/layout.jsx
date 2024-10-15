4ec94c08d8c883bdd658d8736b6eb9433d561ffc
HalimaSaadia <halimasadia9666@gmail.com>
2024-10-15 14:04:29 +0600

    set graph image for website

--- src/app/(withCommonLayout)/layout.jsx
+++ src/app/(withCommonLayout)/layout.jsx
@@ -4,6 +4,8 @@ import Navbar from "@/components/shared/Navbar/Navbar";
 export default function HomeLayout({ children }) {
   return (
     <div>
+       <meta property="og:image" content="https://i.postimg.cc/cJgyCKvm/favicon.png" />
+       <meta property="og:url" content="https://www.twintradeint.com/" />
       <Navbar />
       <div>{children}</div>
       <Footer />
